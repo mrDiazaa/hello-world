1c6b2b612a4cf5e814cf14507d61901a7140392a
ArtDiaz <artdiaz2@gmail.com>
2020-11-24 17:48:45 -0800

    Commiting Hello World file

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+
+	public static void main(String[] args) {
+		// Beginning program to print Hello World
+		System.out.println("Hello World!");
+		System.out.println("Adding another HelloWorld line");
+
+	}
+
+}
